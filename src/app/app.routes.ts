b7cc8d3e1c686ee7e869ad26fa525176aee46437
Mohammed <mohammedsuhailr2@gmail.com>
2026-06-01 18:03:34 +0200

    refactor: move routes to app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
+// app.routes.ts
 import { Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home.component';
+import { ClientsComponent } from './features/clients/clients.component';
+import { NursesComponent } from './features/nurses/nurses.component';
+import { InboxComponent } from './features/inbox/inbox.component';
+import { LoginComponent } from './features/login/login.component';
+import { EditDataComponent } from './features/edit-data/edit-data.component';
+import { ViewDataComponent } from './features/view-data/view-data.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', title: 'Dashboard', component: HomeComponent },
+  { path: 'clients', title: 'Clients', component: ClientsComponent },
+  { path: 'nurses', title: 'Nurses', component: NursesComponent },
+  { path: 'inbox', title: 'Inbox', component: InboxComponent },
+  { path: 'login', title: 'Login', component: LoginComponent },
+  { path: 'edit', title: 'Edit', component: EditDataComponent },
+  { path: 'view', title: 'View', component: ViewDataComponent },
+];
